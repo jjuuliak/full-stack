087c2b1037e16c97147ab7a151a1b02b6f41360b
Juulia Ketola <juulia.ketola@tuni.fi>
2022-03-18 16:21:31 +0200

    1.13 done

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -12,13 +12,28 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(7).fill(0))
+
+  const setVote = () => {
+    const copy = { ...votes }
+    copy[selected] += 1
+    setVotes(copy)
+  }
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
+      <p>
+        {anecdotes[selected]} <br/>
+        has {votes[selected]} votes
+      </p>
+
+      <button onClick={() => setVote()}>
+          vote
+      </button>
+
       <button onClick={() => setSelected(Math.floor(Math.random() * (anecdotes.length)))}>
           next anecdote
-       </button>
+      </button>
     </div>
   )
 }
